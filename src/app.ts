0bb805d5615ccb5bba606150c35ff954ce90784d
Edgar Borges <ed9arborges@gmail.com>
2025-09-26 21:52:19 +0100

    fix: healthcheck

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,19 @@ import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 
 const app = express()
+
+// Simple CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
